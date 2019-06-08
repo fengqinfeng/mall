ace46f750ad6bdeb5daea15f0fbce76723abae79
fqf <937438477@qq.com>
2019-06-08 19:37:14 +0800

    打包成jar之前

--- src/main/java/com/mall/MallApplication.java
+++ src/main/java/com/mall/MallApplication.java
@@ -6,7 +6,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 //@EnableTransactionManagement
 @MapperScan("com.mall.mapper") //扫描的mapper
@@ -23,5 +27,15 @@ public class MallApplication {
         //SpringUtil.printBean();
 
     }
+    @Bean
+    public WebMvcConfigurer webMvcConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
+    }
+
 
 }
--- src/main/java/com/mall/service/Recommend.java
+++ src/main/java/com/mall/service/Recommend.java
@@ -14,36 +14,6 @@ import java.io.IOException;
 import java.util.List;
 @Service
 public class Recommend {
-//    public static void main(String[] args) throws Exception {
-//        //准备数据 这里是电影评分数据
-//        File file = new File("ratings.dat");
-//        //将数据加载到内存中，GroupLensDataModel是针对开放电影评论数据的
-//        DataModel dataModel = new FileDataModel(new File("C:\\D\\课件\\企业级javaee\\推荐算法学习\\ml-latest-small\\ra.csv"));
-//
-//
-//
-//        //计算相似度，相似度算法有很多种，欧几里得、皮尔逊等等。
-//        ItemSimilarity itemSimilarity = new PearsonCorrelationSimilarity(dataModel);
-//        //构建推荐器，协同过滤推荐有两种，分别是基于用户的和基于物品的，这里使用基于物品的协同过滤推荐
-//        GenericItemBasedRecommender recommender = new GenericItemBasedRecommender(dataModel, itemSimilarity);
-//        //给用户ID等于5的用户推荐10个与2398相似的商品
-//        List<RecommendedItem> recommendedItemList = recommender.recommendedBecause(1, 6, 10);
-//        //打印推荐的结果
-//        System.out.println("使用基于物品的协同过滤算法");
-//        System.out.println("根据用户5当前浏览的商品2398，推荐10个相似的商品");
-//        for (RecommendedItem recommendedItem : recommendedItemList) {
-//            System.out.println(recommendedItem);
-//        }
-//        long start = System.currentTimeMillis();
-//        //recommendedItemList = recommender.recommendedBecause(5, 32, 10);
-//        //打印推荐的结果
-////        System.out.println("使用基于物品的协同过滤算法");
-////        System.out.println("根据用户5当前浏览的商品32，推荐10个相似的商品");
-////        for (RecommendedItem recommendedItem : recommendedItemList) {
-////            System.out.println(recommendedItem);
-////        }
-//        System.out.println(System.currentTimeMillis() -start);
-//    }
     public List<RecommendedItem> recommend(long userid,long itemid)throws Exception{
         DataModel dataModel=null;
         try {
@@ -52,6 +22,7 @@ public class Recommend {
             dataModel = new FileDataModel(new File("C:\\D\\课件\\企业级javaee\\推荐算法学习\\ml-latest-small\\ra.csv"));
             //计算相似度
             ItemSimilarity itemSimilarity = new PearsonCorrelationSimilarity(dataModel);
+            //System.out.println(itemSimilarity.allSimilarItemIDs(6).toString());
             //构建推荐器，协同过滤推荐有两种，分别是基于用户的和基于物品的，这里使用基于物品的协同过滤推荐
             GenericItemBasedRecommender recommender = new GenericItemBasedRecommender(dataModel, itemSimilarity);
             //给用户ID等于5的用户推荐10个与2398相似的商品
